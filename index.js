f6d545797e9affaebff1be5b1275c3ff96d82f7c
MANIMARAN K <126849398+MANIMARAN2002@users.noreply.github.com>
2024-01-01 10:07:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let tem1=document.getElementById('pas').addEventListener('input',(event)=>{
+    //console.log(event.target.value)
+    
+})
+
+
+function slo(){
+    let name1=document.getElementById("myname").value;
+    console.log(name1);
+    let name2=document.getElementById("mail").value;
+    console.log(name2);
+    let name3=document.getElementById("pas").value;
+    console.log(name3);
+}
